057c54c58d6497f562d5e114230d974fdc10154c
kdevocht <kevin_devocht@byu.edu>
2016-12-27 14:50:59 -0700

    add closed support for slack

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -5,8 +5,9 @@ var http = require("http");
 
 var mealInfo = [];
 var dateRange = [];
+var cannonClosed = "Sorry, but the Commons is closed all day";
 
-function mealData(mealname, mealid, servedate){
+function mealData(mealname, mealid, servedate) {
     this.mealname = mealname;
     this.mealid = mealid;
     this.servedate = servedate;
@@ -14,35 +15,35 @@ function mealData(mealname, mealid, servedate){
 
 
 
-module.exports.menu = function(req, res){
+module.exports.menu = function (req, res) {
     console.log(req.body);
     var meal = req.body.result.parameters['meal-type'];
     var date = getDate(req.body['timestamp']);
     var cached = false;
     var matchedMeal;
-    for(i=0; i<mealInfo.length; i++){
-        if(mealInfo[i].mealname === meal && mealInfo[i].servedate ===date){
-            cached =true;
+    for (i = 0; i < mealInfo.length; i++) {
+        if (mealInfo[i].mealname === meal && mealInfo[i].servedate === date) {
+            cached = true;
             matchedMeal = i;
             break;
         }
     }
-    if(!cached){
+    if (!cached) {
         getMealIds(date, req, res);
     }
-    else{
+    else {
         getMenu(mealInfo[matchedMeal], res);
     }
-    
-     
+
+
 };
 
 
-function getMealIds(date, req, res){
+function getMealIds(date, req, res) {
     var options = {
         host: 'dining.byu.edu',
         port: 80,
-        path: '/commons/menu_pass.php?servedate='+date+'&viewname=MenusJSON&_=1457029106452',
+        path: '/commons/menu_pass.php?servedate=' + date + '&viewname=MenusJSON&_=1457029106452',
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -50,17 +51,17 @@ function getMealIds(date, req, res){
         }
     };
 
-    var req2 = http.get(options, function(res2){
+    var req2 = http.get(options, function (res2) {
         // Buffer the body entirely for processing as a whole.
         var bodyChunks = [];
-        res2.on('data', function(chunk) {
+        res2.on('data', function (chunk) {
             // You can process streamed parts here...
             bodyChunks.push(chunk);
-        }).on('end', function() {
+        }).on('end', function () {
             var body = Buffer.concat(bodyChunks);
             body = JSON.parse(body);
-            if(body['menus'].length > 0){
-                body['menus'].forEach(function(meal){
+            if (body['menus'].length > 0) {
+                body['menus'].forEach(function (meal) {
                     mealInfo.push(new mealData(meal.mealname, meal.mealid, meal.servedate));
                 })
                 module.exports.menu(req, res);
@@ -71,12 +72,12 @@ function getMealIds(date, req, res){
 }
 
 
-function getMenu(mealData, res){
+function getMenu(mealData, res) {
 
     var options = {
         host: 'dining.byu.edu',
         port: 80,
-        path: '/commons/menu_pass.php?servedate='+mealData.servedate+'&mealname='+mealData.mealname+'&mealid='+mealData.mealid+'&viewname=MenuItemsJSON',
+        path: '/commons/menu_pass.php?servedate=' + mealData.servedate + '&mealname=' + mealData.mealname + '&mealid=' + mealData.mealid + '&viewname=MenuItemsJSON',
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -89,13 +90,13 @@ function getMenu(mealData, res){
     var fusion = [];
     var grainery = [];
     var grill = [];
-    var req = http.get(options, function(res2){
+    var req = http.get(options, function (res2) {
         // Buffer the body entirely for processing as a whole.
         var bodyChunks = [];
-        res2.on('data', function(chunk) {
+        res2.on('data', function (chunk) {
             // You can process streamed parts here...
             bodyChunks.push(chunk);
-        }).on('end', function() {
+        }).on('end', function () {
             var body = Buffer.concat(bodyChunks);
             body = JSON.parse(body);
             //getEvents(res, body, processMenu);
@@ -104,14 +105,14 @@ function getMenu(mealData, res){
             var speechString;
             var displayString;
             var menuException = false;
-            for(i=0; i<items.length; i++){
-                if(items[i].description === "The Commons is Closed All Day"){
-                    speechString = items[i].description;
-                    displayString = items[i].description;
+            for (i = 0; i < items.length; i++) {
+                if (items[i].description === "The Commons is Closed All Day") {
+                    speechString = cannonClosed;
+                    displayString = cannonClosed;
                     menuException = true;
                     break;
                 }
-                switch(items[i].category){
+                switch (items[i].category) {
                     case "Euro":
                         euro.push(items[i].description);
                         break;
@@ -129,66 +130,70 @@ function getMenu(mealData, res){
                         break;
                 }
             }
+            if (!menuException) {
+                var slack_message = {
+                    "text": "Menu for " + mealData.servedate,
+                    "attachments": [
+                        {
+                            "title": mealData.mealname,
+                            "title_link": 'http://dining.byu.edu/commons/menus.html',
+                            "color": "#36a64f",
 
-    var slack_message = {
-        "text": "Menu for "+mealData.servedate,
-        "attachments": [
-                    {
-                        "title": mealData.mealname,
-                        "title_link": 'http://dining.byu.edu/commons/menus.html',
-                        "color": "#36a64f",
+                            "fields": [
+                                {
+                                    "title": "Fusion",
+                                    "value": fusion.toString(),
+                                    "short": true
+                                },
+                                {
+                                    "title": "Expo",
+                                    "value": expo.toString(),
+                                    "short": true
+                                },
+                                {
+                                    "title": "Euro",
+                                    "value": euro.toString(),
+                                    "short": true
+                                },
+                                {
+                                    "title": "Grill",
+                                    "value": grill.toString(),
+                                    "short": true
+                                }
+                            ],
 
-                        "fields": [
-                            {
-                                "title": "Fusion",
-                                "value": fusion.toString(),
-                                "short": true
-                            },
-                            {
-                                "title": "Expo",
-                                "value": expo.toString(),
-                                "short": true
-                            },
-                            {
-                                "title": "Euro",
-                                "value": euro.toString(),
-                                "short": true
-                            },
-                                                {
-                                "title": "Grill",
-                                "value": grill.toString(),
-                                "short": true
-                            }
-                        ],
-
-                    }
-                ]
+                        }
+                    ]
+                }
             }
-            if(!menuException){
-                speechString =  "At the Fusion station they are serving,"+fusion.toString()+
-                    ".  At the Expo station They are serving,"+expo.toString()+
-                    ". At the Euro station they are serving"+euro.toString()+
-                    ". At the Grill they are serving"+grill.toString()
-                displayString = "At the Fusion station they are serving,"+fusion.toString()+
-                    ".\n At the Expo station They are serving,"+expo.toString()+
-                    ".\n At the Euro station they are serving"+euro.toString()+
-                    ".\n At the Grill they are serving"+grill.toString()
+            else{
+                var slack_message = {"text": cannonClosed}
+            }
+            if (!menuException) {
+                speechString = "At the Fusion station they are serving," + fusion.toString() +
+                    ".  At the Expo station They are serving," + expo.toString() +
+                    ". At the Euro station they are serving" + euro.toString() +
+                    ". At the Grill they are serving" + grill.toString()
+                displayString = "At the Fusion station they are serving," + fusion.toString() +
+                    ".\n At the Expo station They are serving," + expo.toString() +
+                    ".\n At the Euro station they are serving" + euro.toString() +
+                    ".\n At the Grill they are serving" + grill.toString()
             }
             res.setHeader('Content-type', 'application/json');
-            res.send(JSON.stringify({speech:speechString, displayText: displayString, data: {"slack": slack_message},contextOut: [],source: "The Cannon Center Menu"}));
+            res.send(JSON.stringify({ speech: speechString, displayText: displayString, data: { "slack": slack_message }, contextOut: [], source: "The Cannon Center Menu" }));
 
 
         });
         req.end();
-        
+
     });
 }
 
-function processMenu(res, menu, events){
+function processMenu(res, menu, events) {
 
-    for(var i = 0; i<menu['menus'][0]['recipes'].length; i++){
+    for (var i = 0; i < menu['menus'][0]['recipes'].length; i++) {
         var eventFired = false;
-        for(var j = 0; j<events.length; j++) {
+        for (var j = 0; j < events.length; j++) {
             if (menu['menus'][0]['recipes'][i]['shortname'] === events[j]['Name']) {
                 console.log("Event in For Loop: " + JSON.stringify(events[j]));
                 sendNotification(menu['menus'][0]['servedate'], menu['menus'][0]['mealname'], events[j]);
@@ -196,7 +201,7 @@ function processMenu(res, menu, events){
                 break
             }
         }
-        if(!eventFired){
+        if (!eventFired) {
             registerEvents(menu['menus'][0]['servedate'], menu['menus'][0]['mealname'], menu['menus'][0]['recipes'][i]['shortname'], menu['menus'][0]['recipes'][i]['description']);
 
         }
@@ -205,31 +210,31 @@ function processMenu(res, menu, events){
     res.send(menu);
 }
 
-function dateCreator(date, meal){
+function dateCreator(date, meal) {
 
     var mealStart;
     var mealEnd;
 
-    var year = date.slice(0,4);
-    var month =date.slice(4, 6);
+    var year = date.slice(0, 4);
+    var month = date.slice(4, 6);
     var day = date.slice(-2);
-    var formattedDate = year+'-'+month+'-'+day+'T';
+    var formattedDate = year + '-' + month + '-' + day + 'T';
 
     return formattedDate;
 
 }
 
-function getDate(queryDate){
+function getDate(queryDate) {
     var DateString = queryDate.split('T')[0].replace(/-/g, '');
     return (DateString);
 }
 
 function getMonday(weekAdjuster) {
     d = new Date();
-    d.setDate(d.getDate() +weekAdjuster);
+    d.setDate(d.getDate() + weekAdjuster);
 
     var day = d.getDay(),
-        diff = d.getDate() - day + (day == 0 ? 1:8); // adjust when day is sunday
+        diff = d.getDate() - day + (day == 0 ? 1 : 8); // adjust when day is sunday
     var date = new Date(d.setDate(diff));
 
     return date;
